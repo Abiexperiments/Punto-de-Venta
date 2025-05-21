134ea63c001ecdb22e7a2de6bb36d8a5859ffd43
Abiexperiments <abigailgarcia900200@gmail.com>
2025-05-21 06:36:05 -0600

    se agrega msj

--- src/Controlador/PuntoDeVentaMVC.java
+++ src/Controlador/PuntoDeVentaMVC.java
@@ -10,6 +10,7 @@ public class PuntoDeVentaMVC {
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             try {
+            	System.out.println("prueba");
                 VistaLogin vistaLogin = new VistaLogin();
                 vistaLogin.setVisible(true);
                 new ControladorLogin(vistaLogin);
